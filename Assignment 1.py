410fff4900ba6d95eb70cc1fe10068e50228b355
Raushan Kumar <er_raushan@outlook.com>
2025-11-01 13:21:10 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,18 @@
+# Task 1
+
+
+
+a=float(input("Enter 1st Number:"))
+b=float(input("Enter 2nd Number:"))
+
+print("Addition:",a+b)
+print("substraction:",a-b)
+print("Multiplication",a*b)
+print("Division",a/b)
+
+# Task 2
+
+first_name=input("Enter your First Name:")
+last_name=input("Enter Your Last Name:")
+
+print("Hello,",first_name+" " +last_name+"!",   "Welcome to The Python Program.")
