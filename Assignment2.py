1fe2706f8bd2e135bebbd30d40bf48eaa430289c
Raushan Kumar <er_raushan@outlook.com>
2025-11-06 12:12:03 +0530

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,19 @@
+# Task 1
+
+# number=int(input("Enter a Number:"))
+# if number%2==0: 
+#     print(f"{number} is an Even Number")
+
+# else: 
+#     print(f"{number} is an Odd Number")
+
+# Task 2
+
+
+print("To Get The Integer Sum between 2 Numbers ")
+startNumber=int(input("Enter starting Number:"))
+EndNumber=int(input("Enter starting Number:"))
+sum=0
+for i in range(startNumber,EndNumber+1,1):
+    sum=sum+i
+print(f"The Sum of numbers from {startNumber} to {EndNumber} : {sum}")
