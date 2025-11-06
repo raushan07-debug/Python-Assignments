6cf7171c6db2ece0c6ec7c848394ff1f23454f31
Raushan Kumar <er_raushan@outlook.com>
2025-11-06 12:13:49 +0530

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -1,11 +1,11 @@
 # Task 1
 
-# number=int(input("Enter a Number:"))
-# if number%2==0: 
-#     print(f"{number} is an Even Number")
+number=int(input("Enter a Number:"))
+if number%2==0: 
+     print(f"{number} is an Even Number")
 
-# else: 
-#     print(f"{number} is an Odd Number")
+else: 
+     print(f"{number} is an Odd Number")
 
 # Task 2
 
@@ -16,4 +16,5 @@ EndNumber=int(input("Enter starting Number:"))
 sum=0
 for i in range(startNumber,EndNumber+1,1):
     sum=sum+i
+
 print(f"The Sum of numbers from {startNumber} to {EndNumber} : {sum}")
